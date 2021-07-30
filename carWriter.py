afaf5a96dba2054d450f55e7999e11d078abc08f
osmiiin <milnavsoc@gmail.com>
2021-07-30 21:40:36 +0900

    final
    final

---
+++ carWriter.py
@@ -0,0 +1,8 @@
+import csv
+
+with open('차량관리.csv','w') as file:
+    csv_maker = csv.writer(file, delimiter=',')
+    csv_maker.writerow([1, '08러1234', '2020-10-20,14:00'])
+    csv_maker.writerow([2, '25다1234', '2020-10-20,14:10'])
+    csv_maker.writerow([3, '28하1234', '2020-10-20,14:20'])
+print('차량관리.csv 파일이 생성되었습니다.')
--- copyfile.py
+++ copyfile.py
@@ -1,7 +1,7 @@
 buffer_size = 1024
 
-with open('BTS_P2D.mp4','rb') as source:
-    with open('BTS_P2D.mp4','wb') as copy:
+with open('bts_P2D.mp4','rb') as source:
+    with open('bts_P2D.mp4','wb') as copy:
         while True:
             buffer = source.read(buffer_size)
             if not buffer: 
